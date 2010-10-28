790b94a32955162ec0ed31da2f5d4f23021493a2
presbrey <presbrey@gmail.com>
2010-10-28 19:40:42 +0000

    clarify HTTP header basic docs

--- www/wildcard/index.php
+++ www/wildcard/index.php
@@ -6,9 +6,9 @@ include_once('header.php');
 <p>Specify desired response types with a method-supported HTTP Accept header.</p>
 <hr />
 <p>In the identifers used below:</p>
-<dl><dt>*/</dt><dd>refers to a media type you specify in an HTTP header</dd>
-<dt>?=</dt><dd>refers to a query string and is application/x-www-form-urlencoded</dd></dl>
-<p>Some query string options and response (HTTP Accept) media types are combinable.</p>
+<dl><dt>*/type</dt><dd>refers to a media type 'type' specified in an HTTP header</dd>
+<dt>?k=v</dt><dd>refers to a query string parameter 'k' with value 'v': passed in URL via GET or application/x-www-form-urlencoded via POST</dd></dl>
+<p>Some query string options and response (HTTP Accept) media types are complementary.</p>
 <h3>HTTP methods supported:</h3>
 <ul>
 <li>GET: RDF, SPARQL (?query=), JSON (*/json), JSON-P (?callback=), RSS (*/rss+xml), Atom (*/atom+xml)</li>
