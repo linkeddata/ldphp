3cf170d76ab8c76ff8cd5489e3ebc056a9c510bc
presbrey <presbrey@gmail.com>
2010-11-10 23:31:43 +0000

    CORS: allow Credentials, Headers, and Origin all fixed for end-to-end test

--- www/inc/wildcard.inc.php
+++ www/inc/wildcard.inc.php
@@ -16,3 +16,21 @@ if (!strstr($_filename, '/')) {
 }
 
 header("X-User: $_user");
+
+if (!isHTTPS()) {
+    header('Access-Control-Allow-Origin: *');
+} else {
+    header('Access-Control-Allow-Credentials: true');
+    $_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+    $t = explode('/', $_origin);
+    if (count($t) > 2) {
+        $_origin = "{$t[0]}//{$t[2]}";
+    } else {
+        $_origin = '*';
+    }
+    header('Access-Control-Allow-Origin: '.$_origin);
+}
+if ($_SERVER['REDIRECT_REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit;
+}
