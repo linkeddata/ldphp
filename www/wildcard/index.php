2c5f40a565bfa74b143b9cc47e5f542696727300
presbrey <presbrey@gmail.com>
2010-12-02 17:28:34 +0000

    wildcard/index: directory+graph autoindex

--- www/wildcard/index.php
+++ www/wildcard/index.php
@@ -7,32 +7,24 @@
 
 include_once('header.php');
 ?>
-<p>Specify RDF graph by HTTP Document URI; 404s are prepopulated with an empty RDF container.</p>
-<p>Specify request media types with a method-supported HTTP Content-Type header.</p>
-<p>Specify desired response types with a method-supported HTTP Accept header.</p>
 <hr />
-<p>In the identifers used below:</p>
-<dl><dt>*/type</dt><dd>refers to a media type 'type' specified in an HTTP header</dd>
-<dt>?k=v</dt><dd>refers to a query string parameter 'k' with value 'v': passed in URL via GET or application/x-www-form-urlencoded via POST</dd></dl>
-<p>Some query string options and response (HTTP Accept) media types are complementary.</p>
-<h3>HTTP methods supported:</h3>
+<h2>Index for <?=$_SERVER['REQUEST_URI']?></h2>
 <ul>
-<li>GET: RDF, SPARQL (?query=), JSON (*/json), JSON-P (?callback=), RSS (*/rss+xml), Atom (*/atom+xml)</li>
-<li>POST: RDF, SPARQL 1.1 Update (*/sparql-query)</li>
-<li>PUT: RDF</li>
-<li>DELETE</li>
-</ul>
-<h3>RDF media types supported:</h3>
-<ul>
-<li>N3: */rdf+n3, */n3, */turtle</li>
-<li>NTriples: */rdf+nt, */nt</li>
-<li>RDF/XML: */rdf+xml</li>
-<li>RDFa: */html, */xhtml</li>
+<?php
+$listing = scandir($_filename);
+if (count(explode('/', $_base)) <= 4) {
+    $listing = array_slice($listing, 2);
+} else {
+    $listing = array_slice($listing, 1);
+}
+foreach($listing as $item) {
+    if (is_dir("$_filename/$item")) {
+        $item = "$item/";
+    }
+    printf('<li><a href="%s">%s</a></li>', $item, $item);
+}
+?>
 </ul>
 <?php
-echo '<h3>RDF cloud metadata:</h3><pre>';
-print_r($_domain_data);
-echo '</pre>';
-
 TAG(__FILE__, __LINE__, '$Id$');
 include_once('footer.php');
