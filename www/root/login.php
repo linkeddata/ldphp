73a6c959b0aadb45053c207e22934f368dc0cfb5
presbrey <presbrey@gmail.com>
2011-03-27 01:33:14 +0000

    login: add facebook graph callback

--- www/root/login.php
+++ www/root/login.php
@@ -4,4 +4,24 @@
  *
  * $Id$
  */
-header('Location: https://'.BASE_DOMAIN.'/');
+
+if (isset($i_display) && $i_display == 'popup') {
+    $next = newQSA(array('display'=>NULL));
+    echo "<script>opener.document.location = '$next';window.close();</script>";
+    exit;
+}
+if (isset($i_id) && $i_id == 'facebook' && isset($i_session)) {
+    $session = json_decode($i_session, true);
+    if (isset($session['access_token'])) {
+        $q = json_decode(file_get_contents('https://graph.facebook.com/me?fields=id,name,link,username&access_token='.$session['access_token']), true);
+        if (isset($q['id'])) {
+            sess('f:access_token', $session['access_token']);
+            sess('f:access_expires', $session['expires']);
+            foreach ($q as $k=>$v) {
+                sess('f:'.$k, $v);
+            }
+        }
+    }
+}
+
+header('Location: https://'.BASE_DOMAIN.'/manage');
