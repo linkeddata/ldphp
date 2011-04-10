a00692c42fbbef6c6b2d02866d11827bdb55d52c
presbrey <presbrey@gmail.com>
2011-04-10 21:13:15 +0000

    clouds/401: tweak body

--- www/inc/401.php
+++ www/inc/401.php
@@ -17,9 +17,9 @@ please restart your browser to trigger its certificate UI
 </div>
 
 <div class="clear left info">
-We strongly recommend using recent <a target="_blank" href="http://getfirefox.org/">Firefox</a> (&gt;=3.5) with the <a target="_blank" href="http://tabulator.org/">Tabulator extension</a>
+We strongly recommend using <a target="_blank" href="http://getfirefox.org/">Firefox</a> (3.5+)
 <br /><br />
-Shipping Safari/IE and developer Chrome builds are also known to work
+Current shipping Safari/IE and Chrome builds are also known to work
 </div>
 
 <div style="clear:both"></div>
