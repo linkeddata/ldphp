3b69178e24a3b780b866ec947cadbd2b34ca7565
Joe Presbrey <presbrey+git@gmail.com>
2011-11-02 17:57:35 +0000

    footer: add version strings

--- www/inc/footer.php
+++ www/inc/footer.php
@@ -27,7 +27,13 @@ $src = array_slice($src, array_search('www', $src));
 $src = implode('/', $src);
 $src = "https://github.com/presbrey/data.fm/tree/master/$src";
 ?>
-<span id="codeID" style="display: none;"><a href="<?=$src?>"><?=$caller['id']?></a></span>
+<span id="codeID" style="display:none;"><a href="<?=$src?>"><?=$caller['id']?></a>
+/ <?php echo implode(' / ', array(
+    'librdf: '.array_shift(explode(' ',librdf_version_string_get())),
+    'raptor: '.array_shift(explode(' ',raptor_version_string_get())),
+    'rasqal: '.array_shift(explode(' ',rasqal_version_string_get()))
+)); ?>
+</span>
 <span id="codeTime" onclick="$('codeID').toggle();">generated in <?=substr($time, 0, 6)?>s
 <?=$sparql_n<1?'':sprintf('with %d quer%s in %ss', $sparql_n, $sparql_n>1?'ies':'y', substr($sparql_t, 0, 6))?></span>
 </address>
