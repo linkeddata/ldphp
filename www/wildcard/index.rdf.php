86a6a9164c1dc47da2083066de8777fdf1f7d7d4
Joe Presbrey <presbrey+git@gmail.com>
2011-11-17 22:21:02 +0000

    index: bugfix autogen

--- www/wildcard/index.rdf.php
+++ www/wildcard/index.rdf.php
@@ -12,13 +12,12 @@ $g = new \RDF\Graph('memory', '', '', $_base);
 $listing = array();
 if (is_dir($_filename))
     $listing = scandir($_filename);
-if (count(explode('/', $_base)) <= 4) {
-    $listing = array_slice($listing, 2);
-} else {
-    $listing = array_slice($listing, 1);
-}
 foreach($listing as $item) {
-    if ($item[0] == '.') continue;
+    $len = strlen($item);
+    if (!$len) continue;
+    if (($_request_path == '/' && $item == '..') ||
+        ($item[0] == '.' && $item != '..' && substr($item, 0, 5) != '.meta'))
+        continue;
     $is_dir = is_dir("$_filename/$item");
     $item_ext = strrpos($item, '.');
     $item_ext = $item_ext ? substr($item, 1+$item_ext) : '';
