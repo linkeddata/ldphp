9716d3d1baa8149463fc0310277a01c295306834
Joe Presbrey <presbrey+git@gmail.com>
2012-01-25 14:44:15 +0000

    skin/dev: use tabulator trunk

--- www/inc/contrib/skin.html.php
+++ www/inc/contrib/skin.html.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link type="text/css" rel="stylesheet" href="https://raw.github.com/linkeddata/tabulator/master/tabbedtab.css" />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://raw.github.com/linkeddata/tabulator/master/js/mashup/mashlib.js"></script>
+<?php
+    if (substr($_SERVER['SERVER_NAME'],0,4) == 'dev.') {
+        $base = '/common/lib/tabulator/';
+    } else {
+        $base = 'http://linkeddata.github.com/tabulator/';
+    }
+    echo '<link type="text/css" rel="stylesheet" href="', $base, 'tabbedtab.css" />';
+    echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>';
+    echo '<script type="text/javascript" src="', $base, 'js/mashup/mashlib.js"></script>';
+?>
 <script>
 jQuery(document).ready(function() {
-    var uri = window.location.href
+    var uri = window.location.href;
     window.document.title = uri;
     var kb = tabulator.kb;
     var subject = kb.sym(uri);
     tabulator.outline.GotoSubject(subject, true, undefined, true, undefined);
+    if (window.history && window.history.pushState)
+        window.history.pushState({}, document.title, uri);
 });
 </script>
 </head>
