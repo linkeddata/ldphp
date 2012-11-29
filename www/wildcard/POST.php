16c40d31cdd11f819851f5b1672626e589597dec
deiu <andrei@fcns.eu>
2012-11-29 18:10:21 -0500

    Added WebID support. Generates both certificate and local profile.

--- www/wildcard/POST.php
+++ www/wildcard/POST.php
@@ -20,6 +20,12 @@ if (empty($_user)) {
     httpStatusExit(403, 'Forbidden');
 }
 
+
+// Generate a WebID if we have a KEYGEN key in the request
+if (($_POST['SPKAC']) && (strlen($_POST['SPKAC']) > 0)) {
+    require_once 'webidgen.php'; // will exit at the end
+} 
+
 // action
 $d = dirname($_filename);
 if (!file_exists($d))
@@ -45,7 +51,7 @@ if ($_method == 'PATCH') {
 } elseif (!empty($_input) && ($g->append($_input, $_data) || 1)) {
     librdf_php_last_log_level() && httpStatusExit(400, 'Bad Request', null, librdf_php_last_log_message());
 
-    // Check to see if we have an LDP element in the request
+    // Check to see if we have LDP data in the request
     require_once('LDP.php');
     $ldp = new LDP($_filename, $_base, $_input, $_data);
     
@@ -56,6 +62,7 @@ if ($_method == 'PATCH') {
     httpStatusExit(201, 'Created');  
 } elseif ($_content_type == 'application/sparql-query') {
     require_once('SPARQL.php');
+// catch requests for new WebID requests
 } else {
     httpStatusExit(406, 'Content-Type ('.$_content_type.') Not Acceptable');
 }
---
+++ www/wildcard/webidgen.php
@@ -0,0 +1,128 @@
+<?php
+
+/* OpenSSL conf */
+// This is the CA password you supplied when you created the CA 
+$pass = '12345';
+// Path to the openssl.cnf file used by the CA
+$conf = $_SERVER["DOCUMENT_ROOT"].'/CA/openssl.cnf';
+
+// Prepare the request
+$name = (isset($_POST['name']))?$_POST['name']:'Anonymous';
+if (isset($_POST['path'])) {
+    $path = $_POST['path'];
+
+    // Exit if we don't have a #
+    if (strpos($path, '#') === false) // missing # 
+        die("You must at least provide a # fragment. For example: #me or #public.");
+
+    // remove the # fragment so we get the profile document path
+    $path_frag = explode('#', $path);
+    $profile = $path_frag[0];
+    $hash = $path_frag[1];
+} else {
+    die('You need to provide a preferred identifier.');
+}
+$email = $_POST['email'];
+$spkac = $_POST['SPKAC'];
+
+// the full WebID URI
+$webid = $_base.$path;
+
+// Remove any whitespace in teh supplied SPKAC and prepare the cert request
+$req = "SPKAC=".str_replace(str_split(" \t\n\r\0\x0B"), '', $spkac);
+$req .= "\nCN=".$name;
+
+// Export the subjectAltName to be picked up by the openssl.cnf file
+$SAN = "URI:".$webid;
+putenv("SAN=$SAN");
+// Export the base dir for CA 
+putenv("CA_BASE=".$_SERVER["DOCUMENT_ROOT"].'/CA');
+
+// Create temporary files to hold the input and output to the openssl call.
+$tmpSPKACfname = "/tmp/SPK" . md5(time().rand());
+$tmpCERTfname  = "/tmp/CRT" . md5(time().rand());
+
+// Write the SPKAC and DN into the temporary file
+$f = fopen($tmpSPKACfname, "w");
+fwrite($f, $req);
+fclose($f);
+
+// TODO - This should be more easily configured
+$command = "openssl ca -config $conf -verbose -batch -notext -spkac $tmpSPKACfname -out $tmpCERTfname -passin pass:'".$pass."' 2>&1";
+
+// Run the command;
+$output = shell_exec($command);
+//echo $output;
+
+// TODO - Check for failures on the command
+if (preg_match("/Data Base Updated/", $output)==0)
+{
+    //echo $req."<br/>\n";
+	//echo "<pre>";
+	//echo $output;
+	//echo "</pre>";
+	// Remove unneeded files    
+    unlink($tmpSPKACfname);
+    unlink($tmpCERTfname);
+	die('Failed to create X.509 Certificate!');
+}
+// Delete the temporary SPKAC file
+unlink($tmpSPKACfname);
+
+// get the modulus for this certificate
+$command = "openssl x509 -inform der -in $tmpCERTfname -modulus -noout";
+$output = explode('=', shell_exec($command));
+$modulus = preg_replace('/\s+/', '', strtolower($output[1]));
+
+// TODO: make sure the user got the cert somehow
+// Everything is done, now send the p12 encoded SSL certificate back to the user
+// notice: it is IMPERATIVE that no html data gets transmitted to the user before the header is sent!
+$length = filesize($tmpCERTfname);	
+header('Last-Modified: ' . date('r+b'));
+header('Accept-Ranges: bytes');
+header('Content-Length: ' . $length);
+header('Content-Type: application/x-x509-user-cert');
+readfile($tmpCERTfname);
+
+// finally delete the temporary CRT file
+unlink($tmpCERTfname);
+
+// Write the new profile to disk
+$r = new \RDF\Graph('', $_filename.'/'.$profile, '', $_base.'/'.$profile);
+if (!$r) { return false; }
+
+// add a PrimaryTopic
+$r->append_objects($_base.'/'.$profile,
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+         array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/PersonalProfileDocument')));
+$r->append_objects($_base.'/'.$profile,
+        'http://xmlns.com/foaf/0.1/primaryTopic',
+         array(array('type'=>'uri', 'value'=>$_base.'/'.$path)));
+         
+// add a foaf:Person
+$r->append_objects($_base.'/'.$path,
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',                    
+        array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/Person')));
+$r->append_objects($_base.'/'.$path,
+        'http://xmlns.com/foaf/0.1/name',
+        array(array('type'=>'literal', 'value'=>$name)));
+$r->append_objects($_base.'/'.$path,
+        'http://www.w3.org/ns/auth/cert#key',
+        array(array('type'=>'bnode', 'value'=>'_:bnode1')));
+$r->append_objects('_:bnode1',
+        'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+        array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/cert#RSAPublicKey')));         
+$r->append_objects('_:bnode1',
+        'http://www.w3.org/ns/auth/cert#modulus',
+        array(array('type'=>'literal', 'value'=>$modulus, 'datatype'=>'http://www.w3.org/2001/XMLSchema#hexBinary')));
+$r->append_objects('_:bnode1',
+        'http://www.w3.org/ns/auth/cert#exponent',
+        array(array('type'=>'literal', 'value'=>'65537', 'datatype'=>'http://www.w3.org/2001/XMLSchema#int')));
+
+$r->save();
+
+//TODO: Write a .meta file with basic ACL
+
+
+exit;
+
