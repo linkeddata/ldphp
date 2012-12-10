9d54381796a08db645a04cdd712c2e70c48107f0
deiu <andrei@fcns.eu>
2012-12-10 12:05:28 -0500

    Fixed the WAC rule names when generating new WebIDs.

--- www/wildcard/webidgen.php
+++ www/wildcard/webidgen.php
@@ -20,9 +20,9 @@ if (isset($_POST['path'])) {
     $profile = $path_frag[0];
     $hash = $path_frag[1];
     
-    if (file_exists($_filename.'/'.$profile) === true)
+    if (file_exists($_filename.$profile) === true)
         die('You must pick a different identifier. <strong>'.
-            $path.'</strong> already exists in the current directory!');
+            $path.'</strong> already exists in <strong>'.$_base.'</strong>.');
 } else {
     die('You need to provide a preferred identifier.');
 }
@@ -30,7 +30,7 @@ $email = $_POST['email'];
 $spkac = $_POST['SPKAC'];
 
 // the full WebID URI
-$webid = $_base.$path;
+$webid = $_base.$_request_path.$path;
 
 /* --- Certificate --- */
 
@@ -98,46 +98,46 @@ unlink($tmpCERTfname);
 /* --- Profile --- */
 
 // Write the new profile to disk
-$profile = new Graph('', $_filename.'/'.$profile, '', $_base.'/'.$profile);
-if (!$profile) { return false; }
+$document = new Graph('', $_filename.'/'.$profile, '', $_base.'/'.$profile);
+if (!$document) { return false; }
 
 // add a PrimaryTopic
-$profile->append_objects($_base.'/'.$profile,
+$document->append_objects($_base.'/'.$profile,
         'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
          array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/PersonalProfileDocument')));
-$profile->append_objects($_base.'/'.$profile,
+$document->append_objects($_base.'/'.$profile,
         'http://xmlns.com/foaf/0.1/primaryTopic',
          array(array('type'=>'uri', 'value'=>$_base.'/'.$path)));
          
 // add a foaf:Person
-$profile->append_objects($_base.'/'.$path,
+$document->append_objects($_base.'/'.$path,
         'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',                    
         array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/Person')));
 // add name
-$profile->append_objects($_base.'/'.$path,
+$document->append_objects($_base.'/'.$path,
         'http://xmlns.com/foaf/0.1/name',
         array(array('type'=>'literal', 'value'=>$name)));
 // add mbox if we have one
 if (strlen($email) > 0) {
-    $profile->append_objects($_base.'/'.$path,
+    $document->append_objects($_base.'/'.$path,
             'http://xmlns.com/foaf/0.1/mbox',
             array(array('type'=>'uri', 'value'=>'mailto:'.$email)));
 }
 // add modulus and exponent as bnode
-$profile->append_objects($_base.'/'.$path,
+$document->append_objects($_base.'/'.$path,
         'http://www.w3.org/ns/auth/cert#key',
         array(array('type'=>'bnode', 'value'=>'_:bnode1')));
-$profile->append_objects('_:bnode1',
+$document->append_objects('_:bnode1',
         'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
         array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/cert#RSAPublicKey')));         
-$profile->append_objects('_:bnode1',
+$document->append_objects('_:bnode1',
         'http://www.w3.org/ns/auth/cert#modulus',
         array(array('type'=>'literal', 'value'=>$modulus, 'datatype'=>'http://www.w3.org/2001/XMLSchema#hexBinary')));
-$profile->append_objects('_:bnode1',
+$document->append_objects('_:bnode1',
         'http://www.w3.org/ns/auth/cert#exponent',
         array(array('type'=>'literal', 'value'=>'65537', 'datatype'=>'http://www.w3.org/2001/XMLSchema#int')));
 // Write graph to disk
-$profile->save();
+$document->save();
 
 /* --- WAC (.meta) --- */
 
@@ -145,53 +145,88 @@ $meta = new Graph('', $_aclbase.'/.meta', '', $_base.'.meta');
 if (!$meta) { return false; }
 
 // Read the .meta file if we already had one
-$meta->load($_base.'/.meta');
-/*
-<#Default>
-    <http://www.w3.org/ns/auth/acl#accessTo> <http://presbrey.data.fm>, <>, <Public> ;
-    <http://www.w3.org/ns/auth/acl#agentClass> <http://xmlns.com/foaf/0.1/Agent> ;
-    <http://www.w3.org/ns/auth/acl#defaultForNew> <Public> ;
-    <http://www.w3.org/ns/auth/acl#mode> <http://www.w3.org/ns/auth/acl#Read> .
-*/    
-if (substr($_base, -1, 1) == '/')
-    $_base = substr($_base, 0, -1);
-// Add the default entry
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#accessTo',                    
-    array(array('type'=>'uri', 'value'=>$_base)));
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#accessTo', 
-    array(array('type'=>'uri', 'value'=>$profile)));
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#accessTo', 
-    array(array('type'=>'uri', 'value'=>'')));
-    
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#agentClass',                    
-    array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/Agent')));
+//$meta->load($_base.'/.meta');
 
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#defaultForNew',                    
-    array(array('type'=>'uri', 'value'=>$profile)));
+// look for an existing default rule and add it if necessary
+$default_query = 'PREFIX acl: <http://www.w3.org/ns/auth/acl#>
+      SELECT ?s WHERE {?s ?p ?o .
+        FILTER (regex(?s, "#Default", "i"))
+    }';
+$default_res = $meta->SELECT($default_query);
+if (isset($default_res['results']['bindings']) && count($default_res['results']['bindings']) == 0) {
+    // Add the default entry if we don't have one
+    // TODO what do we do with existing files/dirs in the root dir? (add rules for them?)
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#accessTo',                    
+        array(array('type'=>'uri', 'value'=>'http://'.$_domain)));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#accessTo',                    
+        array(array('type'=>'uri', 'value'=>'https://'.$_domain)));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#accessTo',
+        array(array('type'=>'uri', 'value'=>$_request_path)));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#accessTo', 
+        array(array('type'=>'uri', 'value'=>'')));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#agentClass',                    
+        array(array('type'=>'uri', 'value'=>'http://xmlns.com/foaf/0.1/Agent')));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#defaultForNew',                    
+        array(array('type'=>'uri', 'value'=>$_request_path)));
+    $meta->append_objects($_base.'.meta#Default',
+        'http://www.w3.org/ns/auth/acl#mode',          
+        array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/acl#Read')));   
+}
 
-$meta->append_objects('#Default',
-    'http://www.w3.org/ns/auth/acl#mode',                    
-    array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/acl#Read')));
+// Add the Read/Write for user over whole domain if it doesn't exist
+$frag_query = 'PREFIX acl: <http://www.w3.org/ns/auth/acl#>
+      SELECT ?s WHERE {?s ?p ?o .
+        FILTER (regex(?s, "#me", "i"))
+    }';
+$frag_res = $meta->SELECT($frag_query);
+if (isset($frag_res['results']['bindings']) && count($frag_res['results']['bindings']) == 0) {
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#accessTo',                    
+        array(array('type'=>'uri', 'value'=>'http://'.$_domain)));  
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#accessTo',                    
+        array(array('type'=>'uri', 'value'=>'https://'.$_domain)));  
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#agent',                    
+        array(array('type'=>'uri', 'value'=>$webid)));
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#defaultForNew',                    
+        array(array('type'=>'uri', 'value'=>'http://'.$_domain)));
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#defaultForNew',                    
+        array(array('type'=>'uri', 'value'=>'https://'.$_domain)));
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#mode',    
+        array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/acl#Read')));
+    $meta->append_objects($_base.'.meta#'.$hash,
+        'http://www.w3.org/ns/auth/acl#mode',    
+        array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/acl#Write')));
+}
+
+// remove the prefixing / from rule name
+if (substr($_request_path, 0, 1) == '/')
+    $_request_path = substr($_request_path, 1, strlen($_request_path));
 
 // Add the profile rules
-$meta->append_objects('#'.$profile,
+$meta->append_objects($_base.'.meta#'.$_request_path,
     'http://www.w3.org/ns/auth/acl#accessTo',                    
-    array(array('type'=>'uri', 'value'=>$profile)));
-    
-$meta->append_objects('#'.$profile,
+    array(array('type'=>'uri', 'value'=>$_request_path)));  
+$meta->append_objects($_base.'.meta#'.$_request_path,
     'http://www.w3.org/ns/auth/acl#agent',                    
     array(array('type'=>'uri', 'value'=>$webid)));
-
-$meta->append_objects('#'.$profile,
+$meta->append_objects($_base.'.meta#'.$_request_path,
+    'http://www.w3.org/ns/auth/acl#defaultForNew',                    
+    array(array('type'=>'uri', 'value'=>$_request_path)));
+$meta->append_objects($_base.'.meta#'.$_request_path,
     'http://www.w3.org/ns/auth/acl#mode',    
     array(array('type'=>'uri', 'value'=>'http://www.w3.org/ns/auth/acl#Write')));
 
-//echo $meta->to_string('turtle');
 // Write graph to disk
 $meta->save();    
 
