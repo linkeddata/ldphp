82969995c57134196e9ca757a628e6f39e71a8d0
presbrey <presbrey+git@gmail.com>
2013-09-29 08:46:21 +0000

    drop Anonymity claim

--- www/inc/runtime.inc.php
+++ www/inc/runtime.inc.php
@@ -77,7 +77,6 @@ foreach (array($_SERVER['REMOTE_USER'], sess('u:id')) as $_user) {
 if (!strlen($_user) && isset($_SERVER['SSL_CLIENT_CERT'])) {
     require_once('webid.lib.php');
     $_user = webid_verify();
-    
     $_webid = webid_getinfo($_user);
     
     if (DEBUG) {
@@ -86,10 +85,8 @@ if (!strlen($_user) && isset($_SERVER['SSL_CLIENT_CERT'])) {
         closelog();
     }
 
-    if (!isSess('u:name')) 
-        sess('u:name', $_webid['name']);
-    if (!isSess('u:pic'))
-        sess('u:pic', $_webid['pic']);
+    sess('u:name', $_webid['name']);
+    sess('u:pic', $_webid['pic']);
 
     if (strlen($_user) && isset($_SERVER['SSL_CLIENT_S_DN_CN']))
         $_user_name = $_SERVER['SSL_CLIENT_S_DN_CN'];
